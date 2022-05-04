5d4743a13b9cc4ea4aee6c381f66960da5395918
Fernando Acosta <fer.acsvi@gmail.com>
2022-05-04 17:05:00 -0500

    Añadidos archivos y configuración para despliegue

--- tabla_periodica/tabla_periodica/settings.py
+++ tabla_periodica/tabla_periodica/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qu)%#klx73i#-nsn^=@n()xn#a%jdw(dk&+1dq=8f(iwxoo*-^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1'
+]
 
 
 # Application definition
@@ -42,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
